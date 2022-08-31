e20a1614f65993f3cd2ea6e6a20088e18a1363d3
asgaralipq <asgaralipq@gmail.com>
2022-09-01 02:23:54 +0530

    Supplier Consumer

--- Implementing-Lambdas/src/org/asgar/lambdas/FirstLambdas.java
+++ Implementing-Lambdas/src/org/asgar/lambdas/FirstLambdas.java
@@ -1,7 +1,21 @@
 package org.asgar.lambdas;
 
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 public class FirstLambdas {
 	public static void main(String[] args) {
 		
+		Supplier<String> supplier = () -> "Hello";
+		
+		String str = supplier.get();
+		System.out.println(str);
+		
+		Consumer<String> consumer = 
+			(String s) -> {
+				System.out.println("Test");
+				System.out.println(s);
+			}; 
+		consumer.accept("Hello");
 	}
 }
